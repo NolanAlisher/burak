364fd8d88d27cf9700d7a0556dc3e2128c466e5a
AlisherMuhammadjonov <ali.java.py@gmail.com>
2025-06-22 17:41:53 +0900

    feat: Task G has been completed.

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,19 @@
-console.log("Hello World");
+console.log("Train.ts Ishga tushdi");
+
+// TASK G:
+
+// Yagona parametrga ega function tuzing.
+// Va bu function parametr orqalik integer ma'lumot turlariga ega bo'lgan bir arrayni qabul qilsin.
+// Ushbu function bizga arrayning tarkibidagi birinchi eng katta qiymatning indeksini qaytarsin.
+
+// MASALAN: getHighestIndex([5, 21, 12, 21 ,8]); return qiladi 1 sonini
+// Yuqoridagi misolda, birinchi indeksda 21 joylashgan.
+// Va bu 21 soni arrayning tarkibidagi birinchi eng katta son hisobladi va bizga uning indeksi 1 qaytadi.
+
+function findHighest(arr: number[]) {
+  const max = Math.max(...arr);
+  return max;
+}
+
+const result = findHighest([5, 21, 12, 21, 8, 44]);
+console.log("Result:", result);
