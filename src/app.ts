bb95e8fb4c21763537a7c46b4c9ff1404600ebda
AlisherMuhammadjonov <ali.java.py@gmail.com>
2025-06-27 16:42:15 +0900

    fix: modify routing system and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,22 @@
 import express from "express";
 import path from "path";
 import router from "./router";
+import routerAdmin from "./routerAdmin";
 
 // 1-Entrance
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // 2-Session
+
 // 3-Views
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
 // 4-Routers
-app.use("/", router); //MidlleWare Design pattern
+app.use("/admin", routerAdmin); // EJS
+app.use("/", router); // REACT
 
 export default app;
