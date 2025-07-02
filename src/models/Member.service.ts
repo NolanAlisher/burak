d11c61bf27ec16de98f06d86e1455c5ae7930ac5
AlisherMuhammadjonov <ali.java.py@gmail.com>
2025-07-02 14:17:44 +0900

    fix: modify login and signup process

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -3,6 +3,7 @@ import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import { MemberType } from "../libs/enums/member.enum";
 import { NOTFOUND } from "dns";
+import * as bcrypt from "bcryptjs";
 
 class MemberService {
   private readonly memberModel;
@@ -16,6 +17,10 @@ class MemberService {
       .exec();
 
     if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+
+    const salt = await bcrypt.genSalt();
+    input.memberPassword = await bcrypt.hash(input.memberPassword, salt);
+
     try {
       const result = await this.memberModel.create(input);
       result.memberPassword = "";
@@ -34,7 +39,11 @@ class MemberService {
       .exec();
     if (!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
 
-    const isMatch = input.memberPassword === member.memberPassword;
+    const isMatch = await bcrypt.compare(
+      input.memberPassword,
+      member.memberPassword
+    );
+    //const isMatch = input.memberPassword === member.memberPassword;
     console.log("isMatch:", isMatch);
 
     if (!isMatch) {
