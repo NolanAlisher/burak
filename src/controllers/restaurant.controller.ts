c5f27e3000611171610f527bd08202965af0b3e6
AlisherMuhammadjonov <ali.java.py@gmail.com>
2025-07-04 15:43:11 +0900

    fix: organise spa and ssr business logic

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -4,6 +4,8 @@ import MemberService from "../models/Member.service";
 import { LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
 
+const memberService = new MemberService();
+
 const restaurantController: T = {};
 restaurantController.goHome = (req: Request, res: Response) => {
   try {
@@ -11,6 +13,15 @@ restaurantController.goHome = (req: Request, res: Response) => {
     res.send("Home Page");
   } catch (err) {
     console.log("Error, goHome", err);
+  }
+};
+
+restaurantController.getSignup = (req: Request, res: Response) => {
+  try {
+    console.log("getSignup");
+    res.send("Signup Page");
+  } catch (err) {
+    console.log("Error, getSignup", err);
   }
 };
 
@@ -24,41 +35,34 @@ restaurantController.getLogin = (req: Request, res: Response) => {
   }
 };
 
-restaurantController.getSignup = (req: Request, res: Response) => {
-  try {
-    console.log("getSignup");
-    res.send("Signup Page");
-  } catch (err) {
-    console.log("Error, getSignup", err);
-  }
-};
-
-restaurantController.processLogin = async (req: Request, res: Response) => {
-  try {
-    console.log("processLogin");
-    const input: LoginInput = req.body;
-
-    const memberService = new MemberService();
-    const result = await memberService.processLogin(input);
-
-    res.send(result);
-  } catch (err) {
-    console.log("Error, processLogin", err);
-    res.send(err);
-  }
-};
 restaurantController.processSignup = async (req: Request, res: Response) => {
   try {
     console.log("processSignup");
 
     const newMember: MemberInput = req.body;
     newMember.memberType = MemberType.RESTAURANT;
-
-    const memberService = new MemberService();
     const result = await memberService.processSignup(newMember);
+
+    // TODO: Sessions
+
     res.send(result);
   } catch (err) {
     console.log("Error, processSignup", err);
+    res.send(err);
+  }
+};
+
+restaurantController.processLogin = async (req: Request, res: Response) => {
+  try {
+    console.log("processLogin");
+    const input: LoginInput = req.body,
+      result = await memberService.processLogin(input);
+
+    // TODO: Sessions
+
+    res.send(result);
+  } catch (err) {
+    console.log("Error, processLogin", err);
     res.send(err);
   }
 };
--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -2,14 +2,55 @@ import MemberModel from "../schema/Member.model";
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import { MemberType } from "../libs/enums/member.enum";
-import { NOTFOUND } from "dns";
 import * as bcrypt from "bcryptjs";
 
 class MemberService {
   private readonly memberModel;
+
   constructor() {
     this.memberModel = MemberModel;
   }
+
+  /** SPA */
+
+  public async signup(input: MemberInput): Promise<Member> {
+    const salt = await bcrypt.genSalt();
+    input.memberPassword = await bcrypt.hash(input.memberPassword, salt);
+
+    try {
+      const result = await this.memberModel.create(input);
+      result.memberPassword = "";
+
+      return result.toJSON();
+    } catch (err) {
+      console.error("Error, model:signup", err);
+      throw new Errors(HttpCode.BAD_REQUEST, Message.USED_NICK_PHONE);
+    }
+  }
+
+  public async login(input: LoginInput): Promise<Member> {
+    // TODO: Consider member status later
+    const member = await this.memberModel
+      .findOne(
+        { memberNick: input.memberNick },
+        { memberNick: 1, memberPassword: 1 }
+      )
+      .exec();
+    if (!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
+
+    const isMatch = await bcrypt.compare(
+      input.memberPassword,
+      member.memberPassword
+    );
+
+    if (!isMatch) {
+      throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
+    }
+
+    return await this.memberModel.findById(member._id).lean().exec();
+  }
+
+  /** SSR */
 
   public async processSignup(input: MemberInput): Promise<Member> {
     const exist = await this.memberModel
@@ -24,6 +65,7 @@ class MemberService {
     try {
       const result = await this.memberModel.create(input);
       result.memberPassword = "";
+
       return result;
     } catch (err) {
       throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
@@ -43,16 +85,13 @@ class MemberService {
       input.memberPassword,
       member.memberPassword
     );
-    //const isMatch = input.memberPassword === member.memberPassword;
-    console.log("isMatch:", isMatch);
 
     if (!isMatch) {
       throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
     }
 
-    const result = await this.memberModel.findById(member._id).exec();
-    console.log("result:", result);
-    return result;
+    return await this.memberModel.findById(member._id).exec();
   }
 }
+
 export default MemberService;
