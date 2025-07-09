07701bf7c187fbe83a34e95f3f7c3d2f988b3800
AlisherMuhammadjonov <ali.java.py@gmail.com>
2025-07-09 14:56:19 +0900

    feat: product schema model

--- src/schema/Product.model.ts
+++ src/schema/Product.model.ts
@@ -1,3 +1,61 @@
-class ProductService {}
+import mongoose, { Schema } from "mongoose";
+import {
+  ProductVolume,
+  ProductStatus,
+  ProductCollection,
+  ProductSize,
+} from "../libs/enums/product.enum";
 
-export default ProductService;
+const productSchema = new Schema(
+  {
+    productStatus: {
+      type: String,
+      enum: ProductStatus,
+      default: ProductStatus.PAUSE,
+    },
+    productCollection: {
+      type: String,
+      enum: ProductCollection,
+      required: true,
+    },
+    productName: {
+      type: String,
+      required: true,
+    },
+
+    productPrice: {
+      type: String,
+      required: true,
+    },
+
+    productLeftCount: {
+      type: Number,
+      required: true,
+    },
+    productSize: {
+      type: String,
+      enum: ProductSize,
+      default: ProductVolume.ONE,
+    },
+    productDesc: {
+      type: String,
+      required: true,
+    },
+    productImages: {
+      type: [String],
+      default: [],
+    },
+    productViews: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+productSchema.index(
+  { productName: 1, productSize: 1, productVolume: 1 },
+  { unique: true }
+);
+
+export default mongoose.model("Product", productSchema);
