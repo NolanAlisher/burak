ffb6db952618c99968bf92af4e661349f3308fec
AlisherMuhammadjonov <ali.java.py@gmail.com>
2025-07-09 13:10:32 +0900

    feat: locals variable integration

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -16,7 +16,11 @@ routerAdmin.get("/logout", restaurantController.logout);
 routerAdmin.get("/check-me", restaurantController.checkAuthSession);
 
 // PRODUCT
-routerAdmin.get("/product/all", productController.getAllProducts);
+routerAdmin.get(
+  "/product/all",
+  restaurantController.verifyRestaurant,
+  productController.getAllProducts
+);
 routerAdmin.post("/product/create", productController.createNewProduct);
 routerAdmin.post("/product/:id", productController.updateChosenProduct);
 // USER
