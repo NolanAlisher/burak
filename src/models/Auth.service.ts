54bcb6c2b40fd330aee512b0309ea89827916816
AlisherMuhammadjonov <ali.java.py@gmail.com>
2025-08-08 19:28:45 +0900

    feat: develop verify auth user cridential

--- src/models/Auth.service.ts
+++ src/models/Auth.service.ts
@@ -4,7 +4,11 @@ import { Member } from "../libs/types/member";
 import jwt from "jsonwebtoken";
 
 class AuthService {
-  constructor() {}
+  private readonly secretToken;
+
+  constructor() {
+    this.secretToken = process.env.SECRET_TOKEN as string;
+  }
   public createToken(payload: Member) {
     return new Promise((resolve, reject) => {
       const duration = `${AUTH_TIMER}h`;
@@ -23,6 +27,15 @@ class AuthService {
         }
       );
     });
+  }
+
+  public async checkAuth(token: string): Promise<Member> {
+    const result: Member = (await jwt.verify(
+      token,
+      this.secretToken
+    )) as Member;
+    console.log(`----- auth.memberNick: ${result.memberNick} ------`);
+    return result;
   }
 }
 export default AuthService;
