9f07cbeebb4e552a8ca52f2dcf0a43fa8ae93c12
AlisherMuhammadjonov <ali.java.py@gmail.com>
2025-08-08 20:17:32 +0900

    fix: modify getMemberDetail business logic

--- src/router.ts
+++ src/router.ts
@@ -10,5 +10,9 @@ router.post(
   memberController.verifyAuth,
   memberController.logout
 );
-router.get("/member/detail", memberController.verifyAuth);
+router.get(
+  "/member/detail",
+  memberController.verifyAuth,
+  memberController.getMemberDetail
+);
 export default router;
